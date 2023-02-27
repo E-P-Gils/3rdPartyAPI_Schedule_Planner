5a2f75ed0f688e869cd8b4b47ec4ba10c1a2bfd0
unknown <sculptoid11@gmail.com>
2023-02-27 01:21:05 -0500

    Finished function to display local storage on the site

--- assets/script.js
+++ assets/script.js
@@ -8,13 +8,23 @@ $(document).ready(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+  
   //Step One: Created an event listener to add the hour and text input as a key value pair to local storage when the save button is clicked. 
-      $('button').on('click', function(){
-        console.log("Save button clicked!")
-          var scheduledItem = $(this).siblings(".description").val(); 
-          var timeColumn = $(this).parent().attr("id");
-          localStorage.setItem(timeColumn, scheduledItem); 
-      })
+  $('button').on('click', function () {
+    var scheduledItem = $(this).siblings(".description").val();
+    var timeColumn = $(this).parent().attr("id");
+    localStorage.setItem(timeColumn, scheduledItem);
+  })
+  //Step Two: We create a function here to retrieve the local storage key by defining it as a variable, then using find to find the text portion of the time columns and set map the value from local storage onto it. We iterate this process for each time column using jQuery's "each", which is kind of like a more powerful for loop. The "this" is key here as it allows us to act on the entire time column element using a single word.  
+  function saveSchedule() {
+    $(".time-block").each(function () {
+      var timeColumn = $(this).attr("id");
+      var scheduledItem = localStorage.getItem(timeColumn);
+      $(this).find(".description").val(scheduledItem);
+    })
+  }
+  saveSchedule(); 
+  //Step Three: Grab the current time and map it to the html. 
     
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
